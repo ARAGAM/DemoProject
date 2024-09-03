6bdb6ab7d9b093a8802288c2a7fe40bb82ea38e8
ARAGAM <ARAVIND.RAGAM@CAPGEMINI.COM>
2024-09-03 15:27:15 +0530

    second

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -3,6 +3,8 @@ package com.example.demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hi this is aravind");
+		System.out.println("welcome to github");
+		
 	}
 
 }
